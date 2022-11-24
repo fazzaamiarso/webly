e13525e432fb2593f3d9673c4130071a1c1563c4
fazzaamiarso <fazzaamiarso@gmail.com>
2022-11-24 17:50:23 +0700

    layout the hompage

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -3,7 +3,10 @@ import { useLoaderData } from "@remix-run/react";
 import { prisma } from "~/lib/prisma.server";
 
 export const loader = async () => {
-  const webinars = await prisma.webinar.findMany();
+  const webinars = await prisma.webinar.findMany({
+    take: 10,
+    include: { Tickets: true },
+  });
   return json(webinars);
 };
 
@@ -11,26 +14,35 @@ export default function Index() {
   const webinars = useLoaderData<typeof loader>();
 
   return (
-    <main className="w-11/12 mx-auto">
-      <ul className="flex items-center gap-8 flex-wrap">
-        {webinars.map((w) => {
-          return (
-            <li key={w.id} className="w-max">
-              <div className="h-40 w-52">
-                <img
-                  src={w.coverImg}
-                  alt=""
-                  className="rounded-md bg-cover bg-center"
-                />
-              </div>
-              <div className="flex w-full justify-between gap-8">
-                <p>{w.name}</p>
-                <span className="">$20</span>
-              </div>
-            </li>
-          );
-        })}
-      </ul>
+    <main className="w-11/12 mx-auto space-y-12">
+      <section className="w-full h-80 bg-gradient-to-bl from-green-300 via-yellow-300 to-pink-300"></section>
+      <section>
+        <h2 className="font-bold mb-8 text-lg">Featured Webinars</h2>
+        <ul className="flex items-center gap-8 flex-wrap">
+          {webinars.map((w) => {
+            return (
+              <li key={w.id} className="basis-[22%] space-y-2">
+                <div className="h-40 w-full overflow-hidden rounded-md">
+                  <img
+                    src={w.coverImg}
+                    alt=""
+                    className="bg-cover bg-center bg-no-repeat"
+                  />
+                </div>
+                <div className="flex w-full justify-between gap-8">
+                  <div>
+                    <p>{w.name}</p>
+                    <p className="text-sm">Webinar host</p>
+                  </div>
+                  <span className="">
+                    ${w.Tickets.reduce((acc, curr) => acc + curr.price, 0)}
+                  </span>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      </section>
     </main>
   );
 }
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,4 @@
-import { Category, PrismaClient, TicketType } from "@prisma/client";
+import { Category, Prisma, PrismaClient, TicketType } from "@prisma/client";
 import { faker } from "@faker-js/faker";
 
 const prisma = new PrismaClient();
@@ -8,25 +8,40 @@ const randomCategory = () =>
 
 const randomType = () => faker.helpers.arrayElement(Object.values(TicketType));
 
+const fakeWebinars = faker.datatype.array(15).map((_) => {
+  return {
+    id: faker.database.mongodbObjectId(),
+    coverImg: faker.image.food(),
+    name: faker.commerce.productName(),
+    description: faker.commerce.productDescription(),
+    type: randomType(),
+    endDate: faker.date.future(),
+    startDate: faker.date.future(),
+    registrationClosed: faker.date.future(),
+    category: randomCategory(),
+  } as Prisma.WebinarCreateManyInput;
+});
+
 const runSeed = async () => {
   await prisma.webinar.deleteMany();
   console.log("🌱 Start Seeding!");
 
   console.log("Creating Webinars!");
   await prisma.webinar.createMany({
-    data: faker.datatype.array(15).map((_) => {
-      return {
-        coverImg: faker.image.food(),
-        name: faker.commerce.productName(),
-        description: faker.commerce.productDescription(),
-        type: randomType(),
-        endDate: faker.date.future(),
-        startDate: faker.date.future(),
-        registrationClosed: faker.date.future(),
-        category: randomCategory(),
-      };
-    }),
+    data: fakeWebinars,
   });
+  console.log("Creating Webinars Done!");
+  console.log("Creating Tickets!");
+  for (let w of fakeWebinars) {
+    await prisma.ticket.create({
+      data: {
+        webinarId: w.id as string,
+        description: faker.commerce.productDescription(),
+        stock: faker.datatype.number(10),
+        price: faker.datatype.number(30),
+      },
+    });
+  }
 };
 
 runSeed()
